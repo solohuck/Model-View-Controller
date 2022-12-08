f38477510c8c855b0ffff75754bb1c0eec2c498d
solo <solohuckstep@gmail.com>
2022-12-07 23:46:34 -0500

    added code for working server - handlebars and sequelizeStore;

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-
 const routes = require('./controllers');
+
 const sequelize = require('./config/connections');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({}); // starts handlebar engine 
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+  
+  app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
